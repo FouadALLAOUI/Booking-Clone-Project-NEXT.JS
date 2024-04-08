65302ef63b78830182bfa251ea959629fc9c9cb2
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-08 00:31:30 +0000

    Add trending data separately

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,35 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-[#013B94]">
-       <section className="">
-        <h2 className="font-bold text-5xl text-white">Find your Next Stay</h2>
-        <h3 className="text-white py-5 text-xl">
-            Search low prices on hotels, homes and much more...
-        </h3>
-       </section>
+      <section className="max-w-7xl mx-auto p-6">
+       <h2 className="font-bold text-5xl text-white">Find your Next Stay</h2>
+       <h3 className="text-white py-5 text-xl">
+           Search low prices on hotels, homes and much more...
+       </h3>
+      </section>
+
+      <section className="m-4 mt-0 -mb-14 px-2 lg:px-4">
+        {/* SearchForm */}
+      </section>
+
+      <section className="mx-auto max-w-7xl mt-10 p-6 bg-hite rounded-t-lg">
+        <div className="pt-5">
+          <h3 className="text-xl font-bold">Trending Destinations</h3>
+          <p>
+            Most Popular choices for travellers from around the world
+          </p>
+        </div>
+        
+        <div className="flex space-x-4 py-5 overflow-x-scroll">
+            
+        </div>
+
+
+      </section>
+
+
+
+
     </main>
   );
 }
---
+++ data/trending.ts
@@ -0,0 +1,44 @@
+export const trending_data = [
+    {
+        id: 1,
+        src: "https://r-xx.bstatic.com/xdata/images/city/526x420/977220.jpg?k=ee4b7b42c35b8cbf09c8ddb7630092b40cd706fec153c41904ed6e252a883938&o=",
+        title: "Dubai",
+        location: "United Arab Emirates",
+        description: "15 Deals",
+    },
+    {
+        id: 2,
+        src: "https://cf.bstatic.com/xdata/images/city/540x540/653256.jpg?k=db29effa75f45141e936338a82b0ca45d1a970acf280216cb28ae1a785ab447a&o=",
+        title: "Quebec",
+        location: "Canada",
+        description: "30 Deals",
+    },
+    {
+        id: 3,
+        src: "https://cf.bstatic.com/xdata/images/city/540x540/653253.jpg?k=c2a39047e9b056a6c4a90812b40ba11c6455a6f3c6be82731fb108674bfbdf6d&o=",
+        title: "Ottawa",
+        location: "Canada",
+        description: "16 Deals",
+    },
+    {
+        id: 4,
+        src: "https://cf.bstatic.com/xdata/images/xphoto/540x540/288594543.webp?k=7a96a2b4190146f63068bd0604a916c0c885c4899a527e24a9b39487d4ae50b8&o=",
+        title: "South Korea",
+        location: "Asia",
+        description: "31 Deals",
+    },
+    {
+        id: 5,
+        src: "https://cf.bstatic.com/xdata/images/city/540x540/971990.jpg?k=6d52fe4a57a984e2d540e3d7a1910f8a76fda3a57708faddd74e2109c3344b5e&o=",
+        title: "Montreal",
+        location: "Canada",
+        description: "30 Deals",
+    },
+    {
+        id: 6,
+        src: "https://cf.bstatic.com/xdata/images/city/540x540/653240.jpg?k=6b015a87c8443039a685038e97dd58dab6a8748078948b27cca7d1a5fcef308d&o=",
+        title: "Niagara",
+        location: "Canada",
+        description: "25 Deals",
+    },
+]
