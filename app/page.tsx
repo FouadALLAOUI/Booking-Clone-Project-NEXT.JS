37cb417b004d0c0ab27124b7690f5054cbbfc670
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-08 00:38:18 +0000

    Map the trending data in the page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { trending_data } from "@/data/trending";
+import { it } from "node:test";
 
 export default function Home() {
   return (
@@ -23,7 +25,19 @@ export default function Home() {
         </div>
         
         <div className="flex space-x-4 py-5 overflow-x-scroll">
-            
+          {trending_data.map(item =>(
+            <div key={item.id} className="space-y-1 shrink-0 cursor-pointer">
+              <img
+                key={item.id}
+                className="w-80 h-72 object-cover rounded-lg pb-2"
+                src={item.src}
+                alt=""
+              />
+              <p className="font-bold">{item.title}</p>
+              <p className="">{item.location}</p>
+              <p className="font-light text-sm">{item.description}</p>
+            </div>
+          ))}            
         </div>
 
 
